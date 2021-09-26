fe3e2437cd0949b31eefe14bac3fd9d8af8a0b8f
thick <thick@172.21.132.106>
2021-09-26 19:16:47 +0900

    final fix

--- src/com/todo/menu/Menu.java
+++ src/com/todo/menu/Menu.java
@@ -8,6 +8,8 @@ public class Menu {
         System.out.println("add - Add a new item");
         System.out.println("del - Delete an existing item");
         System.out.println("edit - Update an item");
+        System.out.println("find <KEYWORD> - find an item containing KEYWORD");
+        System.out.println("find_cate <KEYWORD> - find a category containing KEYWORD");
         System.out.println("ls - List all items");
         System.out.println("ls_name_asc - sort the list by name");
         System.out.println("ls_name_desc - sort the list by name");
